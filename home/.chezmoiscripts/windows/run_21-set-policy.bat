d68a4fc46710110d37b3ccaa5527fa3672f7811d
Peter Schwier <peter.schwier@cgi.com>
2023-06-07 10:56:43 -0500

    Fix path.

--- home/.chezmoiscripts/windows/run_21-set-policy.bat
+++ home/.chezmoiscripts/windows/run_21-set-policy.bat
@@ -1,2 +1,2 @@
-SET "PATH=%UserProfile%\.dotnet\tools:%UserProfile%\.dotnet:%PATH%"
+SET "PATH=%UserProfile%\.dotnet\tools;%UserProfile%\.dotnet;%PATH%"
 %UserProfile%\.dotnet\tools\pwsh.exe -NoProfile -ExecutionPolicy Unrestricted -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser"
