341ee66faaa25a04d9e2782ead0c115da7d2addc
Peter Schwier <peter.schwier@cgi.com>
2023-06-07 10:42:32 -0500

    Update path during batch script.

--- home/.chezmoiscripts/windows/run_21-set-policy.bat
+++ home/.chezmoiscripts/windows/run_21-set-policy.bat
@@ -1,1 +1,2 @@
+SET "PATH=%UserProfile%\.dotnet\tools:%UserProfile%\.dotnet:%PATH%"
 %UserProfile%\.dotnet\tools\pwsh.exe -NoProfile -ExecutionPolicy Unrestricted -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser"
