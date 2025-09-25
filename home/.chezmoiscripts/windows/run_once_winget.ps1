19c3fadf9bddd8a5951e8b63ac991b1866f96b66
Peter Schwier <peter.schwier@cgi.com>
2025-09-25 10:10:04 -0400

    Add home/.chezmoiscripts/windows/winget.ps1 Add home/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/startup.bat - Shortcut.lnk

---
+++ home/.chezmoiscripts/windows/run_once_winget.ps1
@@ -0,0 +1,9 @@
+winget install Git.Git --silent --accept-package-agreements --accept-source-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
+
+# # browsers
+# winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
+# winget install Mozilla.Firefox --silent --accept-package-agreements --accept-source-agreements
+
+# # dev tools and frameworks
+# winget install Microsoft.PowerShell --silent --accept-package-agreements --accept-source-agreements
+# winget install Vim.Vim --silent --accept-package-agreements --accept-source-agreements
