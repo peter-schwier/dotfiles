3c0cd5159d0f75c5eed91bf352d8f092ba9feb76
Peter Schwier <peter.schwier@cgi.com>
2025-09-26 13:09:02 -0400

    Update home/.chezmoiexternal.toml Update home/.chezmoiscripts/windows/winget.ps1

--- home/.chezmoiscripts/windows/run_once_winget.ps1
+++ home/.chezmoiscripts/windows/run_once_winget.ps1
@@ -1,10 +1,29 @@
-winget install Git.Git --silent --accept-package-agreements --accept-source-agreements --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs"""
-winget install --source winget --exact --id JohnMacFarlane.Pandoc
+# Core - Git
+winget install --source winget --silent --accept-package-agreements --accept-source-agreements Git.Git --override "/VerySilent /NoRestart /o:PathOption=CmdTools /Components=""icons,assoc,assoc_sh,gitlfs""" 
 
-# # browsers
-# winget install Google.Chrome --silent --accept-package-agreements --accept-source-agreements
-# winget install Mozilla.Firefox --silent --accept-package-agreements --accept-source-agreements
+# Core - KeePass
+# Install happens in home\.chezmoiexternal.toml.tmpl.
 
-# # dev tools and frameworks
-# winget install Microsoft.PowerShell --silent --accept-package-agreements --accept-source-agreements
-# winget install Vim.Vim --silent --accept-package-agreements --accept-source-agreements
+# Core - Syncthing
+winget install --source winget --silent --accept-package-agreements --accept-source-agreements Syncthing.Syncthing
+
+# Core - TiddlyWiki
+winget install --source winget --silent --accept-package-agreements --accept-source-agreements Rclone.Rclone
+
+# Main - Firefox
+winget install --source winget --silent --accept-package-agreements --accept-source-agreements Mozilla.Firefox
+
+# Main - Google Chrome
+winget install --source winget --silent --accept-package-agreements --accept-source-agreements Google.Chrome
+
+# Main - PowerShell
+# PowerShell 5 preinstalled on all modern windows. Don't bother installing PowerShell 7, it requires admin rights.
+# winget install --source winget --silent --accept-package-agreements --accept-source-agreements Microsoft.PowerShell
+
+# Main - Talon Voice
+# Install happens in home\.chezmoiexternal.toml.tmpl.
+
+# Extra - Pueue
+# Install happens in home\.chezmoiexternal.toml.tmpl.
+
+
